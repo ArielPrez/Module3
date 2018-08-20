4e819e2580b5ff726b10410f1db2757555074744
ArielPrez <arielpgmm@gmail.com>
2018-08-19 23:13:30 -0400

    Iteration 3 - lab-angular-data-binding

--- lab-angular-data-binding/src/app/food-list/food-list.component.ts
+++ lab-angular-data-binding/src/app/food-list/food-list.component.ts
@@ -1,19 +1,26 @@
 import { Component, OnInit, Input } from '@angular/core';
 import foods from '../foods';
 
+interface foodsInterfaz {
+  name: string;
+  calories: number;
+  quantity: number;
+  image: string;
+}
+
 @Component({
   selector: 'app-food-list',
   template: `
-    <p> <h1>Hola</h1> </p>
   `,
   templateUrl: './food-list.component.html',
   styleUrls: ['./food-list.component.css']
 })
 export class FoodListComponent implements OnInit {
-  foods: Object[];
+  foodsArr: Object[];
   newFood: Object = {};
+  todayFoods: foodsInterfaz[];
   key: string;
-  sech: String = 'Search';
+  add: string = 'Add New';
   isInputDisabled: Boolean = false;
   color: Boolean = false;
   isActive = true;
@@ -22,7 +29,7 @@ export class FoodListComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    this.foods = foods;
+    this.foodsArr = foods;
   }
 
   recordAllTheKeyStrokes(event) {
@@ -34,13 +41,22 @@ export class FoodListComponent implements OnInit {
   toggleInput() {
         this.isInputDisabled = !this.isInputDisabled;
         this.isActive = !this.isActive;
-        if (this.isInputDisabled) { this.sech = 'Activate'; } else { this.sech = 'Search'; }
+        if (this.isInputDisabled) {
+          this.add = 'Activate';
+        } else {
+          this.add = 'Add New';
+        }
+        
+        
   }
   changeBorder(){
     this.color = !this.color; 
   } 
 
-  
+  addItem(){
+    this.foodsArr.push(this.newFood);
+    this.newFood = {};
+  }
   
 }
 
